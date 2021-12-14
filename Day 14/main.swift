d0a0dfd8fda01bbbc804be8d39b9e7fc9a50bbff
Stephen H. Gerstacker <stephen@gerstacker.us>
2021-12-14 16:31:56 -0500

    Day 14 - Part 1 + Partial Part 2

---
+++ Day 14/main.swift
@@ -0,0 +1,166 @@
+//
+//  main.swift
+//  Day 14
+//
+//  Created by Stephen H. Gerstacker on 2021-12-14.
+//  Copyright © 2021 Stephen H. Gerstacker. All rights reserved.
+//
+
+import Algorithms
+import Foundation
+
+let data = ExampleData
+
+struct Rule: CustomDebugStringConvertible, Hashable {
+    let input: String
+    let result: String.Element
+    let output: [String]
+
+    init(input: String, result: String.Element) {
+        self.input = input
+        self.result = result
+
+        let firstIndex = input.startIndex
+        let first = String(input[firstIndex])
+
+        let secondIndex = input.index(after: firstIndex)
+        let second = String(input[secondIndex])
+
+        self.output = [
+            "\(first)\(result)",
+            "\(result)\(second)"
+        ]
+    }
+
+    var debugDescription: String {
+        return "\(input) -> [\(output[0]),\(output[1])]"
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(input)
+    }
+}
+
+struct SolverResult {
+    let maxCharacter: String
+    let maxCount: Int
+
+    let minCharacter: String
+    let minCount: Int
+
+    var result: Int {
+        maxCount - minCount
+    }
+}
+
+class Solver {
+
+    let initialPairs: [String]
+    let rules: [String:Rule]
+
+    init(template: String, rules: [String:Rule]) {
+        initialPairs = template.adjacentPairs().map { "\($0.0)\($0.1)" }
+        self.rules = rules
+    }
+
+    func run(steps: Int) -> SolverResult {
+        var currentTotals: [String:Int] = [:]
+
+        for pair in initialPairs {
+            currentTotals[pair] = (currentTotals[pair] ?? 0) + 1
+        }
+
+        for _ in 0 ..< steps {
+            var nextTotals: [String:Int] = [:]
+
+            for key in currentTotals.keys {
+                let rule = rules[key]!
+
+                for pair in rule.output {
+                    nextTotals[pair] = (nextTotals[pair] ?? 0) + 1
+                }
+            }
+
+            currentTotals = nextTotals
+        }
+
+        var finalTotals: [String.Element:Int] = [:]
+
+        for (key, value) in currentTotals {
+            let firstIndex = key.startIndex
+            let first = key[firstIndex]
+
+            finalTotals[first] = (finalTotals[first] ?? 0) + value
+
+            let secondIndex = key.index(after: firstIndex)
+            let second = key[secondIndex]
+
+            finalTotals[second] = (finalTotals[second] ?? 0) + value
+        }
+
+        let (maxCharacter, maxCount) = finalTotals.max { $0.1 < $1.1 }!
+        let (minCharacter, minCount) = finalTotals.min { $0.1 < $1.1 }!
+
+        return SolverResult(
+            maxCharacter: String(maxCharacter),
+            maxCount: maxCount,
+            minCharacter: String(minCharacter),
+            minCount: minCount
+        )
+    }
+}
+
+var lines = data.components(separatedBy: .newlines)
+
+let templateString = lines.removeFirst()
+
+lines.removeFirst()
+
+var rules: [String:Rule] = [:]
+
+for line in lines {
+    let parts = line.split(separator: " ")
+    precondition(parts.count == 3)
+
+    let lhs = String(parts[0])
+    let rhs = String(parts[2])
+
+    let rule = Rule(input: lhs, result: rhs.first!)
+    rules[lhs] = rule
+}
+
+// MARK: - Part 1
+
+let solver1 = Solver(template: templateString, rules: rules)
+let result1 = solver1.run(steps: 10)
+
+print("Part 1")
+print("------")
+print("Max: \(result1.maxCharacter) = \(result1.maxCount)")
+print("Min: \(result1.minCharacter) = \(result1.minCount)")
+print("Result: \(result1.result)")
+
+exit(0)
+
+
+let templatePairs = templateString.adjacentPairs().map { "\($0.0)\($0.1)" }
+
+var currentTotals: [String:Int] = [:]
+
+for pair in templatePairs {
+    currentTotals[pair] = (currentTotals[pair] ?? 0) + 1
+}
+
+for step in 0 ..< 10 {
+    var nextTotals: [String:Int] = [:]
+
+    for key in currentTotals.keys {
+        let rule = rules[key]!
+
+        for pair in rule.output {
+            nextTotals[pair] = (nextTotals[pair] ?? 0) + 1
+        }
+    }
+
+    currentTotals = nextTotals
+}
